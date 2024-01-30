b40fc6690adcbebdf5f916b2652853514d897116
MAximilianRaine <raine.maximilian@gmail.com>
2024-01-30 20:52:35 +0100

    needs styling and fixing the api

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,17 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+    @bookmarks = @list&.bookmarks || []
+    puts "Debug: @list - #{@list.inspect}"
+    puts "Debug: @bookmarks - #{@bookmarks.inspect}"
   end
+
 
   def new
     @list = List.new
@@ -13,13 +19,27 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save
-    redirect_to lists_path(@list)
+
+    if @list.save
+      redirect_to @list, notice: 'List was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path, notice: 'List was successfully deleted.'
   end
 
   private
-
+  def set_list
+    @list = List.includes(:bookmarks).find_by(id: params[:id])
+    unless @list
+      redirect_to lists_path, alert: 'List not found'
+    end
+  end
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :image_url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
-  resources :lists, only: [:index, :show, :new, :create]
-  resources :bookmarks
+  resources :lists
+  resources :lists do
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
 end
